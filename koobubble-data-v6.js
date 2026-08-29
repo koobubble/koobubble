991a51774bb479b1ac2846a796c7d57ca7a24d46
koobubble <del22614@gmail.com>
2026-08-29 20:57:41 +0900

    Add files via upload

---
+++ koobubble-data-v6.js
@@ -0,0 +1,18 @@
+const CHATROOM = {
+  title: "건일 선배",
+  status: "980724"
+};
+
+const SENDER = {
+  name: "건일 선배",
+  profile: "profile.jpg"
+};
+
+const MESSAGES = [
+  {
+    date: "2022-10-21",
+    time: "11:14",
+    type: "text",
+    text: "빌런즈~~! 우리 런즈를 너무 사랑하는 엑디즈 건일이에요 🙂 버블을 통해 서로 더 가깝고 즐겁게 소통할 수 있게 되어서 넘 설레고 기뻐요! 함께 대화하면서 우리 더 친해져봐요!! 잘 부탁드려요 :)"
+  }
+];
